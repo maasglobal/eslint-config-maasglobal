174d676d852fbd26aee0a8d593235d2eba0b27f3
Mariusz Nowak <medyk@medikoo.com>
2019-01-03 12:43:48 +0100

    chore: improve tag script

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -5,7 +5,7 @@ if [ $TRAVIS_BRANCH == 'master' ] && [ -z $TRAVIS_PULL_REQUEST_BRANCH ]
 then
   latest=$(npm info eslint-config-maasglobal dist-tags.latest) &&
   git tag v${latest} &&
-  git push origin v${latest}
+  git push https://maasglobal:$GITHUB_API_KEY@github.com/maasglobal/eslint-config-maasglobal v${latest}
 else
   echo "Package is tagged only from master branch" &&
   exit 1
