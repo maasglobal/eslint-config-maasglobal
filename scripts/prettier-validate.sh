ed739b405238a1cd3bcf0dd325bf97f4aeece720
Mariusz Nowak <medyk@medikoo.com>
2019-01-08 17:09:46 +0100

    fix: ensure to expose error

--- scripts/prettier-validate.sh
+++ scripts/prettier-validate.sh
@@ -4,12 +4,12 @@
 # - Updated against master
 # - Existing (filter out deleted, which are also reported by git diff)
 # - Covered by prettier
-PRETTIER_TARGET=$(git diff --name-only master...HEAD | perl -ne 'chomp(); if (-e $_) {print "$_\n"}' | egrep '\.css$|\.html$|\.js$|\.json$|\.md$|\.yaml$')
+PRETTIER_TARGET=$(git diff --name-only master...HEAD | perl -ne 'chomp(); if (-e $_) {print "$_\n"}' | egrep '\.css$|\.html$|\.js$|\.json$|\.md$|\.yaml$') &&
 
 # Validate only if there are files to validate
 if [ -n "${PRETTIER_TARGET}" ]
 then
-  BROKEN_FILES=$(npx prettier -l $PRETTIER_TARGET)
+  BROKEN_FILES=$(npx prettier -l $PRETTIER_TARGET) &&
   if [ -n "${BROKEN_FILES}" ]
   then
     echo -e "\x1B[0;31mError: Following files do not match Prettier output\n\n${BROKEN_FILES}\x1B[0m\n"
